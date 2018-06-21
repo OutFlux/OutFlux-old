38e4d9255797189e7e117b0e2d4b1c259d9de83d
sauravyash <yash_agasti@hotmail.com>
2018-06-21 22:34:28 +1000

    hi

--- .circleci/testCI.js
+++ .circleci/testCI.js
@@ -14,7 +14,6 @@ app.start().then(function () {
   // Verify the window is visible
   assert.equal(isVisible, true);
 }).then(function () {
-  
   // Get the window's title
   return app.client.getTitle()
 }).then(function (title) {
