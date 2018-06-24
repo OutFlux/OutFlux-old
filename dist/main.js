87c484129f30fd792e1f84840290bc4ead7e4bb1
sauravyash <yash_agasti@hotmail.com>
2018-06-25 08:32:48 +1000

    alot of changes

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,14 @@
 'use strict';
 
-var _require = require('electron'),
-    app = _require.app,
-    BrowserWindow = _require.BrowserWindow;
+var _electron = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-
-
 var win = void 0;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new _electron.BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
   win.loadFile('index.html');
@@ -32,18 +28,18 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+_electron.app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+_electron.app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit();
+    _electron.app.quit();
   }
 });
 
-app.on('activate', function () {
+_electron.app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
