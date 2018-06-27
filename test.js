db78668aac5ecdce654582e140d703e2770148ba
sauravyash <yash_agasti@hotmail.com>
2018-06-27 20:54:10 +1000

    ;(

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ app.start().then(() => {
 }).then( isVisible => {
   console.log('Browser Window is visible')
   // Verify the window is visible
-  assert.equal(isVisible, true);
+  assert.equal(isVisible, true)
 }).then( () => {
   // Get the window's title
   return app.client.getTitle()
@@ -21,7 +21,7 @@ app.start().then(() => {
   assert.equal(title, 'OutFlux')
 }).then( () => {
   // Stop the application
-  console.log("Test is Successful!");
+  console.log("Test is Successful!")
   
   return app.stop()
 }).catch(error => 
